7b6a6134134028b06487d68ee2ab5d1bbd9c7122
marcelonfilho <marcelonfilho.ms@gmail.com>
2024-06-26 00:15:44 -0400

    perf: add promise.all to home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,10 @@ import ProductList from "./_components/product-list";
 import SectionTitle from "./_components/section-title";
 import { db } from "./_lib/prisma";
 import RestaurantList from "./_components/restaurant-list";
+import Link from "next/link";
 
-const Home = async () => {
-  const products = await db.product.findMany({
+const fetch = async () => {
+  const getProducts = db.product.findMany({
     where: {
       discountPercentage: {
         gt: 0,
@@ -20,11 +21,35 @@ const Home = async () => {
       restaurant: {
         select: {
           name: true,
-          id: true,
         },
       },
     },
   });
+
+  const getBurgersCategory = db.category.findFirst({
+    where: {
+      name: "Hambúrgueres",
+    },
+  });
+
+  const getPizzasCategory = db.category.findFirst({
+    where: {
+      name: "Pizzas",
+    },
+  });
+
+  const [products, burgersCategory, pizzasCategory] = await Promise.all([
+    getProducts,
+    getBurgersCategory,
+    getPizzasCategory,
+  ]);
+
+  return { products, burgersCategory, pizzasCategory };
+};
+
+const Home = async () => {
+  const { products, burgersCategory, pizzasCategory } = await fetch();
+
   return (
     <>
       <Header />
@@ -37,10 +62,12 @@ const Home = async () => {
       </div>
 
       <div className="px-5 pt-6">
-        <Banner
-          src="/promo-banner-01.png"
-          alt="Até 30% de desconto em pizzas"
-        />
+        <Link href={`/categories/${pizzasCategory?.id}/products`}>
+          <Banner
+            src="/promo-banner-01.png"
+            alt="Até 30% de desconto em pizzas"
+          />
+        </Link>
       </div>
 
       <div className="pt-6 space-y-4">
@@ -51,7 +78,12 @@ const Home = async () => {
       </div>
 
       <div className="px-5 pt-6">
-        <Banner src="/promo-banner-02.png" alt="A partir de 17,90 em lanches" />
+        <Link href={`/categories/${burgersCategory?.id}/products`}>
+          <Banner
+            src="/promo-banner-02.png"
+            alt="A partir de 17,90 em lanches"
+          />
+        </Link>
       </div>
 
       <div className="py-6 space-y-4">
